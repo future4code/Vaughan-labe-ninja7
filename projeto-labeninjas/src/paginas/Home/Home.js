0042421b3d339b414d773e54c5f1cf00f35037bd
SORAIAROCHA <soraiarsr2017@gmail.com>
2022-02-02 21:25:34 +0000

    correcao

--- projeto-labeninjas/src/paginas/Home/Home.js
+++ projeto-labeninjas/src/paginas/Home/Home.js
@@ -7,8 +7,8 @@ class Home extends React.Component {
                 <h1>LabeNinjas</h1>
                 <h3>Tudo o que você precisa a um click de distância</h3>
                 <div>
-                    <button onClick={() => this.props.changePage("form")}>Quero oferecer meu serviço</button>
-                    <button onClick={() => this.props.changePage("list")}>Quero contratar um serviço</button>
+                    <button onClick={() => this.props.changePage("cadastre")}>Quero oferecer meu serviço</button>
+                    <button onClick={() => this.props.changePage("listaJobs")}>Quero contratar um serviço</button>
                 </div>
             </HomePageContainer>
         )
