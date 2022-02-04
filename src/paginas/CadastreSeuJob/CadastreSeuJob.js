e88899d4be145a355128b4d544d778491503a696
ViniciusBarbosa <92874088+viniciuscicone@users.noreply.github.com>
2022-02-04 16:24:00 -0300

    ajuste

--- src/paginas/CadastreSeuJob/CadastreSeuJob.js
+++ src/paginas/CadastreSeuJob/CadastreSeuJob.js
@@ -1,104 +1,111 @@
 import React from "react";
 import styled from "styled-components";
-import { Key } from "react";
-import { BaseUrl } from "react";
+import { BaseUrl } from "../../components/Header/BaseUrl";
 import axios from "axios";
+import { Key } from "../../components/Header/Key";
+
+const ContainerForm = styled.div`
+
+display:flex;
+flex-direction: column;
+align-items: center;
+
+`
 
 export default class CadastreSeuJob extends React.Component {
-
-    Data = (event) => {
-        this.setState({dueDate : event.target.value})
-    }
-
-   Titulo = (event) => {
-        this.setState({title : event.target.value})
-    }
-
-   Descricao = (event) => {
-        this.setState({description : event.target.value})
-    }
-
-   FormaPagamento = (event) => {
-        this.setState({paymentMethods: event.target.value})
-        console.log(this.state.paymentMethods)
-    }
-
-    Preco = (event) => {
-        this.setState({price : event.target.value})
-    }
 
     state = {
 
         title: "",
         description: "",
         price: 0,
-        paymentMethods:[],
+        paymentMethods: [],
         dueDate: "",
     }
 
+
+    Data = (event) => {
+        this.setState({ dueDate: event.target.value })
+    }
+
+    Titulo = (event) => {
+        this.setState({ title: event.target.value })
+    }
+
+    Descricao = (event) => {
+        this.setState({ description: event.target.value })
+    }
+
+    FormaPagamento = (event) => {
+        const pagamento = []
+        pagamento.push(event.target.value)
+        this.setState({ paymentMethods: pagamento });
+    }
+
+    Preco = (event) => {
+        const valor = Number(event.target.value)
+        this.setState({ price: valor })
+    }
+
+
     Cadastro = () => {
-         
-        const url = "https://labeninjas.herokuapp.com/jobs"
 
+        const url = `${BaseUrl}/jobs`
         const body = {
-        title : this.state.title,
-        description : this.state.description,
-        price : this.state.price,
-        paymentMethods : this.state.paymentMethods,
-        dueDate : this.state.dueDate,
+            title: this.state.title,
+            description: this.state.description,
+            price: this.state.price,
+            paymentMethods: this.state.paymentMethods,
+            dueDate: this.state.dueDate,
         }
+        axios.post(url,body,Key)
+            .then((resp) => {
+                alert("serviço cadastrado com sucesso !")
+            })
 
-        axios.post(url, body, {headers: {Authorization:"24dfb346-d6c6-4912-b155-f15dbb9a0f4d"}} )
-
-        .then((resp)=>{
-             alert(resp)
-            console.log(resp.data)
-        })
-
-        .catch((error)=> {
-            alert(`Não foi possivel fazer o cadastro, motivo: ${error}`)
-        })
+            .catch((error) => {
+                console.log(error)
+                alert('Não foi possivel fazer o cadastro, tente novamente')
+            })
     }
 
     render() {
 
         return (
+            <>
+                <ContainerForm>
+                    <h2> Cadastre seu Serviço! </h2>
+                    Serviço
+                    <input value={this.state.title} onChange={this.Titulo} placeholder="Titulo do produto" />
+                    <br></br>
+                    Descricao do serviço
+                    <input value={this.state.description} onChange={this.Descricao} placeholder="Descrição do produto" />
+                    <br></br>
+                    Valor
+                    <input type="number" value={this.state.price} onChange={this.Preco} placeholder="Preço do produto" />
+                    <br></br>
+                    <form>
+                        <select onChange={this.FormaPagamento}>
+                            <option value=""> Escolha </option>
+                            <option value="pix"> PIX </option>
+                            <option value="boleto"> BOLETO </option>
+                            <option value="debito"> DEBITO </option>
+                            <option value="credito"> CREDITO </option>
+                            <option value="paypal"> PAYPAL</option>
+                        </select>
+                        <br></br>
+                    </form>
 
-            <div>
-        <>
-
-            <h2> Cadastre seu Serviço! </h2>
-
-                <input value={this.state.title} onChange={this.Titulo} placeholder="Titulo do produto" />
-            <br></br>
-                <input value={this.state.description} onChange={this.Descricao} placeholder="Descrição do produto" /> 
-                <br></br>
-                <input type="number" value={this.state.price} onChange={this.Preco} placeholder="Preço do produto" />
-                <br></br>
-            <form>
-
-                <select onChange={this.FormaPagamento}>
-
-                <option value="pix"> PIX </option>
-                <option value="boleto"> BOLETO </option>
-                <option value="debito"> DEBITO </option>
-                <option value="credito"> CREDITO </option>
-                <option value="paypal"> PAYPAL</option>
-            
-
-                </select>
-                <br></br>
-            </form>
-
-            <input value={this.state.dueDate} onChange={this.Data} type="date" />
-            <br></br>
-            <button onClick={this.fazCadastro}> Cadastrar Produto </button>
+                    <input value={this.state.dueDate} onChange={this.Data} type="date" />
+                    <br></br>
+                    <button onClick={() => this.Cadastro()}> Cadastrar Produto </button>
+                    <button onClick={() => console.log(this.state)}>clique </button>
+                    
+                </ContainerForm>
+            </>
 
 
-        </>
-      </div>
-  
-      );
+        );
     }
-  }
+}
 
