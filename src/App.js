0516f87b6eb8f4bb5e42a4f2e35e2f455b871fff
ViniciusBarbosa <92874088+viniciuscicone@users.noreply.github.com>
2022-02-05 15:06:06 -0300

    Carrinho adicionado

--- src/App.js
+++ src/App.js
@@ -6,18 +6,58 @@ import DetalheJob from './paginas/DetalheJob/DetalheJob'
 import Carrinho from "./paginas/Carrinho/Carrinho"
 import Header from "./components/Header/Header"
 import axios from "axios"
-
+import { Key } from "./components/Header/Key"
+import { BaseUrl } from "./components/Header/BaseUrl"
 
 class App extends React.Component {
   state = {
     currentPage: "home",
-   
+    jobCarrinho: [],
+    listaJobs: [],
   }
+
+  
+  componentDidMount() {
+
+    this.getAllJobs()
+    
+ }
 
 
   changePage = (pageName) => {
     this.setState({ currentPage: pageName })
   }
+
+  
+  adicionarProduto = (x) => {
+      
+    const job = this.state.jobCarrinho.find(trabalho => x === trabalho.id)
+    if (job) {
+        alert("Ja existente")
+    } else {
+
+       const adicionarJob = this.state.listaJobs.find(trabalho => x === trabalho.id)
+       const novoJob = [...this.state.jobCarrinho, { ...adicionarJob }]
+       this.setState({ jobCarrinho: novoJob })
+
+    }
+ }
+
+
+  getAllJobs = () => {
+  
+
+    const url = `${BaseUrl}/jobs`
+    axios
+       .get(url, Key)
+       .then((respostaPositiva) => {
+          console.log(respostaPositiva)
+          this.setState({ listaJobs: respostaPositiva.data.jobs })
+       })
+       .catch((erro) => {
+          console.log("algo deu errado")
+       })
+ }
 
   choosePage = () => {
     switch (this.state.currentPage) {
@@ -27,11 +67,11 @@ class App extends React.Component {
         cadastrar={() => this.setState({ currentPage: 'cadastre' }) }
         changePage={this.changePage}/>
       case "listaJobs":
-        return <ContrateJob/>
+        return <ContrateJob  entrada={this.state.listaJobs} botao={this.adicionarProduto}/>
       case "cadastre":
         return <CadastreSeuJob />
       case "carrinho":
-        return <Carrinho/>
+        return <Carrinho recept={this.state.jobCarrinho} remover={() => console.log("voce removeu do carrinho")}/>
       case "detalhes":
         return <DetalheJob/>
       default:
--- src/paginas/Carrinho/Carrinho.js
+++ src/paginas/Carrinho/Carrinho.js
@@ -1,16 +1,62 @@
 import React from "react";
+import {ContainerCards, Card} from '../ContrateJob/ContrateJob' 
+import styled from 'styled-components'
 
-  
+const Main = styled.main`
+
+display:grid;
+grid-template-columns: repeat(4, 1fr);
+margin: 5px;
+padding: 5px;
+justify-items: center;
+
+`
+
 export default class Carrinho extends React.Component {
+
+    state = {
+        itens: []
+
+    }
+
+    componentDidMount() {
+
+        this.setState({ itens: this.props.recept })
+    }
+
 
 
 
     render() {
+        console.log(this.state.itens)
+        const ItensCarrinho = this.state.itens.map((x, y) => {
+            return (
+               <ContainerCards key={y}>
+                  <Card>
+                     <h3>{x.title}</h3>
+                     {x.description}
+                     <h3>{x.price.toLocaleString('pt-BR',
+                        { style: 'currency', currency: 'BRL' })} </h3>
+                     Metodo de pagamento: {x.paymentMethods.map((x, y) => {
+                        return (<>
+                           <li key={y}>{x}</li>
+                        </>
+                        )
+                     })}
+                     <br />
+                     {x.dueDate ? <p><strong>Prazo:</strong> {new Date(x.dueDate).toLocaleDateString()}</p> : "Carregando..."}
+                  </Card>
+                  <button onClick={() => this.props.remover(x.id)} >Remover Ninja</button>
+               </ContainerCards>
+            )
+         })
 
+        return (
+            <>
+                <div>NINJAS SELECIONADOS</div>
+               <Main>{ItensCarrinho}</Main>
+            </>
 
-        return(
-
-            <div>ESSE E O CARRINHO</div>
         )
     }
 }
